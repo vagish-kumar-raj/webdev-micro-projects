425e07ec6dcc71451c4de0fb44d5d350b164dfb5
vagish-kumar-raj <230776036+vagish-kumar-raj@users.noreply.github.com>
2026-08-17 20:45:12 +0530

    feat: working tally counter

--- tally-counter/script.js
+++ tally-counter/script.js
@@ -1,1 +1,18 @@
-let count = 0;
+let count = document.getElementById("count")
+
+let X = 0
+
+function add() {
+    X = X + 1
+    count.innerText = X
+}
+
+function sub() {
+    X = X - 1
+    count.innerText = X
+}
+
+function reset() {
+    X = 0
+    count.innerText = X
+}
